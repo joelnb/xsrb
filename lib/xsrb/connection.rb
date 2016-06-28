7cdc97c3fce6f2c8f73ac841dfd5d28d997de9c9
Joel Noyce Barnham <joelnbarnham@gmail.com>
2016-06-28 14:51:05 +0100

    Add some more to Transport classes

--- lib/xsrb/connection.rb
+++ lib/xsrb/connection.rb
@@ -4,5 +4,12 @@ require 'xsrb/utils'
 module XenStore
   # Generic class which implements a connection to a XenStore daemon.
   class Connection
+    # Serialize a +Packet+ and send over the wire to XenStored
+    def send(pkt)
+    end
+
+    # Receive a +Packet+ from XenStored
+    def recv
+    end
   end
 end
--- lib/xsrb/transport.rb
+++ lib/xsrb/transport.rb
@@ -1,16 +1,49 @@
+require 'socket'
+
 module XenStore
   # Module to hold various types of Transport which allow communication
   # with XenStore in various ways.
   module Transport
-    # Superclass for various types of Transport which communicate with
-    # XenStore in various ways.
-    class Transport
-    end
-
+    # A Transport implementation which communicates with XenStore over the
+    # special device on UNIX-like operating systems.
     class XenBusTranspoort
+      def initialize(path = nil)
+        path ||= XenStore::Utils.xenbus_path
+        @fileno = File.open(path, 'w')
+
+        ObjectSpace.define_finalizer(self, proc { @fileno.close })
+      end
+
+      def send(s)
+      end
+
+      def recv
+      end
+
+      def close
+        @fileno.close
+      end
     end
 
+    # A Transport implementation which communicates with XenStore over a
+    # UNIX socket.
     class UnixSocketTransport
+      def initialize(path = nil)
+        path ||= XenStore::Utils.unix_socket_path
+        @sock = UNIXSocket.new path
+
+        ObjectSpace.define_finalizer(self, proc { @sock.close })
+      end
+
+      def send(s)
+      end
+
+      def recv
+      end
+
+      def close
+        @sock.close
+      end
     end
   end
 end
