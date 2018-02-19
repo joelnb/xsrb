0619b79039712dd3aa036bf970dfcd0574b2869a
Joel Noyce Barnham <joelnbarnham@gmail.com>
2018-02-19 22:39:14 +0000

    Add basic initialize method

--- lib/xsrb/client.rb
+++ lib/xsrb/client.rb
@@ -1,5 +1,8 @@
 module XenStore
   # Client provides high-level interaction with XenStore
   class Client
+    def initialize(transport = nil)
+      @transport = transport || XenStore::Connection::UnixSocketConnection.new
+    end
   end
 end
